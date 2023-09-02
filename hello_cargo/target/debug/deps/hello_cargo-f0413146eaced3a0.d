468e3c7eeab83aa7bbbd1140f225b417a804bfba
sasakino <s23427978@gmail.com>
2023-09-02 22:57:32 +0900

    initial commit

---
+++ hello_cargo/target/debug/deps/hello_cargo-f0413146eaced3a0.d
@@ -0,0 +1,5 @@
+/Users/n_sasaki/workspace/private/learn/trpl/hello_cargo/target/debug/deps/hello_cargo-f0413146eaced3a0: src/main.rs
+
+/Users/n_sasaki/workspace/private/learn/trpl/hello_cargo/target/debug/deps/hello_cargo-f0413146eaced3a0.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/n_sasaki/workspace/private/learn/trpl/hello_cargo/target/debug/hello_cargo: /Users/n_sasaki/workspace/private/learn/trpl/hello_cargo/src/main.rs
