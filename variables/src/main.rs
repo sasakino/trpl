899b4cd51f04fdc1c4e03506671597ecca765b8e
sasakino <s23427978@gmail.com>
2023-09-04 07:03:42 +0900

    3.3. 関数

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,45 +1,31 @@
-use std::io;
-
 fn main() {
-    let c = 'z';
-    let z = 'ℤ';
-    let heart_eyed_cat = '😻';    //ハート目の猫
-    println!("c: {}, z: {}, heart_eyed_cat: {}", c, z, heart_eyed_cat);
+    another_function(5, 'h');
 
-    let tup = (500, 6.4, 1);
-    let (x, y, z) = tup;
-    println!("x: {}, y: {}, z: {}", x, y, z);
+    let y = {
+        let x = 3;
+        x + 1
 
-    let x: (i32, f64, u8) = (500, 6.4, 1);
-    let five_hundred = x.0;
-    let six_point_four = x.1;
-    let one = x.2;
-    println!("five_hundred: {}, six_point_four: {}, one: {}",
-        five_hundred, six_point_four, one);
+    };
 
-    let a = [1, 2, 3, 4, 5];
+    println!("The value of y is: {}", y);
 
-    let first = a[0];
-    let second = a[1];
+    let x = five();
 
-    println!("first: {}, second: {}", first, second);
+    println!("The value of x is: {}", x);
 
-    println!("Please enter an array index.");
+    let x = plus_one(5);
 
-    let mut index = String::new();
+    println!("The value of x is: {}", x);
+}
 
-    io::stdin().read_line(&mut index)
-        .expect("Failed to read line");
+fn another_function(x: i32, unit_label: char) {
+    println!("The measument is {}{}", x, unit_label);
+}
 
-    let index: usize = index
-        .trim()
-        .parse()
-        .expect("Index enterd was not a number");
+fn five() -> i32 {
+    5
+}
 
-    let element = a[index];
-
-    println!(
-        "The value of the element at index {} is: {}",
-        index, element
-    );
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
