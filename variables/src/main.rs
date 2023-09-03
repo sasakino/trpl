2941353cd54f2537d158bc5afcc00d7e44f18c61
sasakino <s23427978@gmail.com>
2023-09-03 17:32:15 +0900

    3.2. データ型

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,20 +1,45 @@
+use std::io;
+
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';    //ハート目の猫
+    println!("c: {}, z: {}, heart_eyed_cat: {}", c, z, heart_eyed_cat);
 
-    const MAX_POINTS: u32 = 100_000;
-    println!("The value of MAX_POINTS is: {}", MAX_POINTS);
+    let tup = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("x: {}, y: {}, z: {}", x, y, z);
 
-    let x = 5;
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let five_hundred = x.0;
+    let six_point_four = x.1;
+    let one = x.2;
+    println!("five_hundred: {}, six_point_four: {}, one: {}",
+        five_hundred, six_point_four, one);
 
-    let x = x + 1;
+    let a = [1, 2, 3, 4, 5];
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {}", x);
-    }
+    let first = a[0];
+    let second = a[1];
 
-    println!("The value of x is: {}", x);
+    println!("first: {}, second: {}", first, second);
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin().read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index enterd was not a number");
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
 }
