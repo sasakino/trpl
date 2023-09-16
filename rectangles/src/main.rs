4c2a0646bedc537a44cdd78c83e745e16eaecfdf
sasakino <s23427978@gmail.com>
2023-09-16 17:11:11 +0900

    5.3. メソッド記法

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,17 +4,29 @@ struct Rectangle {
     height: u32,
 }
 
-fn main() {
-    let lect1 = Rectangle { width: 30, height: 50 };
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&lect1)
-    );
-
-    println!("lect1 is {:#?}", lect1);
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    let square = Rectangle::square(3);
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
