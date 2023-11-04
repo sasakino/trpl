74f4adb0a16c4caef341cd438dbe27083dc49971
sasakino <s23427978@gmail.com>
2023-11-05 01:28:37 +0900

    8.3. キーとそれに紐づいた値をハッシュマップに格納する

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,47 +1,50 @@
 fn main() {
-    let mut s = String::new();
+    use std::collections::HashMap;
 
-    let data = "initial contents";
+    let mut scores = HashMap::new();
 
-    let s = data.to_string();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
 
-    let s = "initial contents".to_string();
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
 
-    let s = String::from("inital contents");
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
 
-    let mut s = String::from("foo");
+    let field_name = String::from("Favarite color");
+    let field_value = String::from("Blue");
 
-    let mut s1 = String::from("foo");
-    let s2 = "bar";
-    s1.push_str(s2);
-    println!("s2 is {}", s2);
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
 
-    let mut s = String::from("lo");
-    s.push('l');
+    let mut scores = HashMap::new();
 
-    let s1 = String::from("Hello, ");
-    let s2 = String::from("world!");
-    let s3 = s1 + &s2;
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
 
-    let s1 = String::from("tic");
-    let s2 = String::from("tac");
-    let s3 = String::from("toe");
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
 
-    let s = s1 + "-" + &s2 + "-" + &s3;
-
-    let s1 = String::from("tic");
-    let s2 = String::from("tac");
-    let s3 = String::from("toe");
-
-    let s = format!("{}-{}-{}", s1, s2, s3);
-
-    let s1 = String::from("hello");
-
-    let hello = "Здравствуйте";
-
-    let s = &hello[0..4];
-
-    for c in "नमस्ते".chars() {
-        println!("{}", c);
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
     }
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores);
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
