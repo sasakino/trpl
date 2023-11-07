4b4d6be3eda2ae7e00e18012428fa2baa33bceab
sasakino <s23427978@gmail.com>
2023-11-07 23:58:19 +0900

    9.3. panic!すべきかするまいか

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,6 +2,26 @@ use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
 
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+
+    pub fn value(&self) -> u32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guessing the number!");
 
@@ -23,9 +43,11 @@ fn main() {
             }
         };
 
-        println!("You guessed: {}", guess);
+        let guess = Guess::new(guess);
 
-        match guess.cmp(&secret_number) {
+        println!("You guessed: {}", guess.value());
+
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
