3cead545f6dba0412873c90a980599b5a7fd9882
sasakino <s23427978@gmail.com>
2023-11-23 17:31:52 +0900

    10.3. ライフタイムで参照を検証する

---
+++ lifetime/src/main.rs
@@ -0,0 +1,25 @@
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn main() {
+    let a = "abcd";
+    let b = "abcdefg";
+
+    println!("{}", longest_with_an_announcement(a, b, 1234));
+    println!("{}", longest_with_an_announcement(a, b, "aaaa"));
+}
