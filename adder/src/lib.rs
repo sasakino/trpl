1f0695bf1ebbda783b4551ccb326b2a1ec4a5110
sasakino <s23427978@gmail.com>
2023-11-24 11:29:53 +0900

    11.2. テストの実行のされ方を制御する

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,11 +1,18 @@
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() -> Result<(), String> {
-        if 2 + 2 == 4 {
-            Ok(())
-        } else {
-            Err(String::from("two plus two does not equal four"))
-        }
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    #[ignore]
+    fn expensive_test() {
     }
 }
