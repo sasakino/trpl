1503661976192a5b11ddde0782d9fe6b2a60f173
sasakino <s23427978@gmail.com>
2023-12-01 19:09:12 +0900

    15.3. Dropトレイトで片付け時にコードを走らせる

--- smart_pointer/src/main.rs
+++ smart_pointer/src/main.rs
@@ -1,33 +1,16 @@
-use std::ops::Deref;
-struct MyBox<T>(T);
-
-impl<T> Deref for MyBox<T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        &self.0
-    }
+struct CustomSmartPointer {
+    data: String,
 }
 
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping Custom SmartPointer with data `{}`!", self.data);
     }
-}
-
-fn hello(name: &str) {
-    println!("Hello, {}!", name);
 }
 
 fn main() {
-    let x = 5;
-    let y = MyBox::new(x);
-    let z = &x;
-
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
-    assert_eq!(5, *(z.deref()));
-
-    let m = MyBox::new(String::from("Rust"));
-    hello(&m);
+    let c = CustomSmartPointer { data: String::from("my self") };
+    println!("CustomSmartPointer created.");
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main.");
 }
