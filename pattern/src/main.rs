64a838c811fcdb287d55aabb74e13a2dc198fb3e
sasakino <s23427978@gmail.com>
2023-12-10 18:00:43 +0900

    18.3. パターン記法

--- pattern/src/main.rs
+++ pattern/src/main.rs
@@ -1,50 +1,225 @@
-fn print_coordinate(&(x, y): &(i32, i32)) {
-    println!("Current location: ({}, {})", x, y);
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+struct Point3d {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+enum Greeting {
+    Hello { id: i32 },
+}
+
+fn foo(_: i32, y: i32) {
+    println!("This code only uses the y parameter: {}", y);
 }
 
 fn main() {
-    let favorite_color: Option<&str> = None;
-    let is_tuesday = false;
-    let age: Result<u8, _> = "34".parse();
+    let x = 1;
 
-    if let Some(color) = favorite_color {
-        println!("Using your favorite color, {}, as the background", color);
-    } else if is_tuesday {
-        println!("Tuesday is green day!");
-    } else if let Ok(age) = age {
-        if age > 30 {
-            println!("Using purple as the background color");
-        } else {
-            println!("Using orange as the background color");
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {:?}", y),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {:?}", x, y);
+
+    let x = 1;
+
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    let x = 5;
+
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+
+    let x = 'c';
+
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
+
+    let p = Point { x: 0, y: 7 };
+
+    let Point { x: a, y: b } = p;
+    assert_eq!(0, a);
+    assert_eq!(7, b);
+
+    let Point { x, y } = p;
+    assert_eq!(0, x);
+    assert_eq!(7, y);
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+
+    }
+
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.");
+        },
+        Message::Move { x, y } => {
+            println!(
+                "Move in the x direction {} and in the y direction {}",
+                x,
+                y
+            );
+        },
+        Message::Write(text) => println!("Text message: {}", text),
+        Message::ChangeColor(r, g, b) => {
+            println!(
+                "Change the color to red {}, green {}, and blue {}",
+                r,
+                g,
+                b
+            );
         }
-    } else {
-        println!("Using blue as the background color");
     }
 
-    let mut stack = Vec::new();
+    let points = vec![
+        Point { x: 0, y: 0},
+        Point { x: 1, y: 5},
+        Point { x: 10, y: -3},
+    ];
 
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
+    let sum_of_squares: i32 = points
+        .iter()
+        .map(|&Point { x, y }| x * x + y * y)
+        .sum();
 
-    while let Some(top) = stack.pop() {
-        println!("{}", top);
+    println!("{}", sum_of_squares);
+
+    let ((feet, inches), Point {x, y}) = ((3, 10), Point {x: 3, y: -10 });
+
+    foo(3, 4);
+
+    let mut setting_value = Some(5);
+    let mut new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
     }
 
-    let v = vec!['a', 'b', 'c'];
+    println!("setting is {:?}", setting_value);
 
-    for (index, value) in v.iter().enumerate() {
-        println!("{} is at index {}", value, index);
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("Some numbers: {}, {}, {}", first, third, fifth);
+        }
     }
 
-    let (x, y, z) = (1, 2, 3);
-    let (x, y, _) = (1, 2, 3);
-    let (x, y, ..) = (1, 2, 3);
+    let _x = 5;
+    let y = 10;
 
-    let point = (3, 5);
-    print_coordinate(&point);
+    let s = Some(String::from("Hello!"));
 
-    if let x = 5 {
-        println!("{}", x);
-    };
+    if let Some(_) = s {
+        println!("found a string");
+    }
+
+    println!("{:?}", s);
+
+    let origin = Point3d { x: 0, y: 0, z: 0 };
+
+    match origin {
+        Point3d {x, .. } => println!("x is {}", x),
+    }
+
+    let numbers = (2, 4, 6, 8, 16, 32);
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {}, {}", first, last);
+        }
+    }
+
+    let mut robot_name = Some(String::from("Bors"));
+
+    match robot_name {
+        Some(ref mut name) => *name = String::from("Another name"),
+        None => (),
+    }
+
+    println!("robot_name is: {:?}", robot_name);
+
+    let num = Some(4);
+
+    match num {
+        Some(x) if x < 5 => println!("less than five: {}", x),
+        Some(x) => println!("{}", x),
+        None => (),
+    }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {:?}", n),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {:?}", x, y);
+
+    let x = 4;
+    let y = false;
+
+    match x {
+        4 | 5 | 6 if y => println!("yes"),
+        _ => println!("no"),
+    }
+
+    let greeting = Greeting::Hello { id: 5 };
+
+    match greeting {
+        Greeting::Hello { id: id_variable @ 3..=7 } => {
+            println!("Found an id in range: {}", id_variable);
+        },
+        Greeting::Hello { id: 10..=12 } => {
+            println!("Found an id in another range");
+        },
+        Greeting::Hello { id } => {
+            println!("Found some other id: {}", id)
+        },
+    }
 }
